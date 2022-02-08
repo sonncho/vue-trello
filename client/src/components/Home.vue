2288eef820e007d814f1a05291dfcb42a1211cf0
sonncho <sonncho@github.com>
2022-02-08 17:58:18 +0900

    Board 목록 조회

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -5,7 +5,9 @@
             Board List :
             <div v-if="loading">Loading...</div>
             <div v-else>
-                Api result: {{ apiRes }}
+                <div v-for="b in boards" :key="b.id">
+                    {{ b }}
+                </div>
             </div>
             <ul>
                 <li>
@@ -20,11 +22,13 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     data() {
         return {
             loading: false,
-            apiRes: '',
+            boards: [],
         }
     },
     created() {
@@ -35,24 +39,17 @@ export default {
             //fetchData가 호출되면 상탯값 변경 처리
             this.loading = true
 
-            //req 변수에 javascript의 내장객체 생성
-            const req = new XMLHttpRequest()
-
-            //XMLHttpRequest.open('method', 'URL') : 요청을  초기화
-            req.open('GET', 'http://localhost:3000/health')
-
-            //send함수를 통해 요청을 보낸다.
-            req.send()
-
-            // 이벤트리스너를 통해 로드시에 처리를 정의
-            req.addEventListener('load', () => { //callback으로 load가 완료되면
-                this.loading = false            // loading이 끝났으므로 상태값 false로 변경
-                this.apiRes = {                 // data객체에 정의한 키값 apiRes에 응답된 정보를 담음
-                    status: req.status,         //  status : 200 또는 404등 상태코드
-                    statusText : req.statusText, // statusText : 상태코드 텍스트
-                    response: JSON.parse(req.response) // response : 응답데이터가 문자열로 오기떄문에 json파싱 필요
-                }
-            })
+            axios.get('http://localhost:3000/boards')
+                .then(res => {
+                    this.boards = res.data
+                })
+                .catch(error => { //에러 발생시
+                    // console.log(error)
+                    this.$router.replace('/login') // vue-router에 $router객체를 이용해 로그인페이지로 이동
+                })
+                .finally(() => {
+                    this.loading = false
+                })
         }
     }
 }
