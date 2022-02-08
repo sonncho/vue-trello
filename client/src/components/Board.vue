a86795d11ef134598962f286f0c243ba5e307b20
sonncho <sonncho@github.com>
2022-02-08 10:48:47 +0900

    API setting

--- client/src/components/Board.vue
+++ client/src/components/Board.vue
@@ -1,9 +1,12 @@
 <template>
     <div>
         Board
-        <div>bid : {{ bid }}</div>
-        <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
-        <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+        <div v-if="loading">Loading Board...</div>
+        <div v-else>
+            <div>bid : {{ bid }}</div>
+            <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
+            <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+        </div>
         <hr/>
         <router-view></router-view>
     </div>
@@ -14,12 +17,23 @@ export default {
     data(){
         return {
             bid: 0,
+            loading: true,
         }
     },
     created(){
-        this.bid = this.$route.params.bid
+        this.fetchData()
         // console.log('bid : '+ this.$route.params.bid)
-    }
+    },
+    methods: {
+        fetchData(){
+            this.loading = true
+            //0.5초후에 상태값 loading을 false로 변경
+            setTimeout(() => {
+                this.bid = this.$route.params.bid
+                this.loading = false
+            }, 500);
+        },
+    },
 }
 </script>
 
--- client/src/components/Card.vue
+++ client/src/components/Card.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
         Card
-        <div>cid: {{ cid }}</div>
+        <div v-if="loading">Loading Card...</div>
+        <div v-else>
+            cid: {{ cid }}
+        </div>
     </div>
 </template>
 
@@ -11,16 +14,25 @@ export default {
     data() {
         return {
             cid: 0,
+            loading: false, // loading 초기 상태값(boolean)
         }
     },
     watch: {
-        '$route'() {
-            this.cid = this.$route.params.cid
+				//기존'$route'()함수형태에서 객체형태로 변경
+				//옵션값 추가
+        '$route': {
+            handler: 'fetchData', //fetchData 함수 실행
+            immediate: true, //즉시 실행 옵션
         }
     },
-    created() {
-        //console.log(this.$route.params.cid)
-        this.cid = this.$route.params.cid
+    methods: {
+        fetchData() {
+            this.loading = true
+            setTimeout(() => {
+                this.cid = this.$route.params.cid
+                this.loading = false
+            }, 500);
+        }
     }
 }
 </script>
