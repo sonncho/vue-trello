f0bb1db4918de682d38d92c242a9c93970c508f0
sonncho <sonncho@github.com>
2022-02-08 10:05:27 +0900

    디렉토리 변경

---
+++ client/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="app">
+		<!-- 여기서부터 코드를 시작합니다! -->
+		<nav-bar/>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+	name: 'app',
+	components: { NavBar, },
+	data () {
+		return {
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ client/src/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        Card
+        <div>cid: {{ cid }}</div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            cid: 0,
+        }
+    },
+    watch: {
+        '$route'() {
+            this.cid = this.$route.params.cid
+        }
+    },
+    created() {
+        //console.log(this.$route.params.cid)
+        this.cid = this.$route.params.cid
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
